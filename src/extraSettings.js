64215615e8187dc6df7f2eefdc1cc3af86262b3c
yoesk <111362980+yoesk@users.noreply.github.com>
2022-08-17 20:35:38 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -29,14 +29,13 @@ let setFruits = {
 let fruitQTan = false;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
-let fruitSlowDown = false;
+let fruitSlowDown = false;5
 
 // 点击右上方图标更换水果：false 改为 true 即可
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
-
+let adLink = 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
 
